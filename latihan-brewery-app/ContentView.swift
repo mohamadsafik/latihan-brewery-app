2c90d6df35c196540ab39ffebe47e43cdc497f32
mohamadsafik <safikmohamad14@gmail.com>
2023-11-23 21:06:28 +0700

    Initial Commit

---
+++ latihan-brewery-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  latihan-brewery-app
+//
+//  Created by Safik Mohamad on 11/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ latihan-brewery-app/latihan_brewery_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  latihan_brewery_appApp.swift
+//  latihan-brewery-app
+//
+//  Created by Safik Mohamad on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct latihan_brewery_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
